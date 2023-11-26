c71f5c5c5dd30a5dc7ed381f191ac7c2883e2a65
Jacob Wan <jacobwan840@gmail.com>
2023-11-26 08:01:03 -0800

    wip

--- argument_processor.go
+++ argument_processor.go
@@ -7,67 +7,149 @@ import (
 	"strings"
 )
 
+type optionProcessor interface {
+	processArgs(o commandOption) error
+}
+
+type stringOption struct{}
+
+func (stringOption) processArgs(o commandOption) error {
+	log.Printf("stringOption.ProcessOption")
+	return nil
+}
+
+type fileOption struct{}
+
+func (fileOption) processArgs(o commandOption) error {
+	log.Printf("fileOption.ProcessOption %+v", o)
+	return nil
+}
+
+type boolOption struct{}
+
+func (boolOption) processArgs(o commandOption) error {
+	log.Printf("boolOption.ProcessOption %+v", o)
+	return nil
+}
+
+type subcommandOption func(string, *argProc)
+
+func WithBoolOption(name string) subcommandOption {
+	return func(subcmd string, processor *argProc) {
+		processor.options[subcmd] = boolOption{}
+	}
+}
+
+func WithFileOption(name string) subcommandOption {
+	return func(subcmd string, processor *argProc) {
+		processor.options[subcmd] = fileOption{}
+	}
+}
+
+func WithStringOption(name string) subcommandOption {
+	return func(subcmd string, processor *argProc) {
+		processor.options[subcmd] = stringOption{}
+	}
+}
+
 type ArgumentProcessor interface {
-	AddSubcommand(subcmd string, parser Parser)
+	AddSubcommand(subcmd string, opts ...subcommandOption)
 	Execute() error
 	ExecuteWithArgs(a []string) error
 }
 
 type argProc struct {
-	router map[string]Parser
+	options map[string]optionProcessor
+}
+
+func (processor *argProc) AddSubcommand(subcmd string, opts ...subcommandOption) {
+	for _, o := range opts {
+		o(subcmd, processor)
+	}
+}
+
+type commandOption struct {
+	name  string
+	value string
 }
 
 type commandSpec struct {
 	subcommandList []string
+	options        []commandOption
 }
 
-func (c commandSpec) subcommandKey() string {
-	return strings.Join(c.subcommandList, " ")
+func (s commandSpec) subcommandListAsString() string {
+	return strings.Join(s.subcommandList, " ")
 }
 
-// ExecuteWithArgs implements ArgumentProcessor.
 func (proc argProc) ExecuteWithArgs(a []string) error {
 	log.Printf("args: %+v", a)
-	log.Printf("router: %+v", proc.router)
-
-	commandSpec := commandSpecFromArgs(a[1:])
-	parser, ok := proc.router[commandSpec.subcommandKey()]
-	if !ok {
-		return fmt.Errorf("subcommand key %s not found", commandSpec.subcommandKey())
+	userArgs := parseUserArgs(a[1:])
+	log.Printf("User args: %+v", userArgs)
+	log.Printf("Processor options %+v", proc.options)
+	subcommandOptions := proc.options[userArgs.subcommandListAsString()]
+	if subcommandOptions == nil {
+		return fmt.Errorf("invalid subcommand key %s", userArgs.subcommandListAsString())
 	}
-	log.Printf("Subcommand parser: %+v", parser)
+	log.Printf("Subcommand options: %+v", subcommandOptions)
 	return nil
 }
 
-// Execute implements ArgumentProcessor.
 func (proc argProc) Execute() error {
 	return proc.ExecuteWithArgs(os.Args)
 }
 
-// AddSubcommand implements ArgumentProcessor.
-func (proc argProc) AddSubcommand(subcmd string, parser Parser) {
-	proc.router[subcmd] = parser
-}
-
 func NewArgumentProcessor() ArgumentProcessor {
-	return argProc{
-		router: map[string]Parser{},
+	return &argProc{
+		options: map[string]optionProcessor{},
 	}
 }
 
-type Parser interface {
-	SetBoolOption(opt string, v bool) error
-}
-
-func commandSpecFromArgs(a []string) commandSpec {
+func parseUserArgs(a []string) commandSpec {
 	result := commandSpec{
 		subcommandList: []string{},
 	}
+	nextArgIdx := 0
 	for _, currentArg := range a {
 		if strings.HasPrefix(currentArg, "-") {
 			break
 		}
 		result.subcommandList = append(result.subcommandList, currentArg)
+		nextArgIdx += 1
+	}
+	a = a[nextArgIdx:]
+	log.Printf("After processing subcommand args: %+v", a)
+	for currentArgIdx, currentArg := range a {
+		if !strings.HasPrefix(currentArg, "-") {
+			break
+		}
+		log.Printf("Processing flag: %s", currentArg)
+		option := strings.TrimLeft(currentArg, "-")
+		log.Printf("Bare option: %s", option)
+
+		// Handle -opt=value form
+		optionParts := strings.SplitN(option, "=", 2)
+		log.Printf("Option parts: %s", optionParts)
+		optionName := optionParts[0]
+		log.Printf("Option name: %s", optionName)
+		if len(optionParts) == 2 {
+			result.options = append(
+				result.options,
+				commandOption{name: optionParts[0], value: optionParts[1]},
+			)
+		} else {
+			nextArgIdx := currentArgIdx + 1
+			if len(a) > nextArgIdx {
+				nextArg := a[nextArgIdx]
+				result.options = append(
+					result.options,
+					commandOption{name: optionParts[0], value: nextArg},
+				)
+			} else {
+				log.Printf("No next arg")
+			}
+		}
+		nextArgIdx += 1
 	}
 	return result
 }
--- argument_processor_test.go
+++ argument_processor_test.go
@@ -5,26 +5,46 @@ import (
 	"testing"
 
 	"github.com/jakewan/argle"
-	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
 	log.SetFlags(0)
 }
 
-type testParser struct {
-	mock.Mock
+type processorSetupFunc = func(argle.ArgumentProcessor)
+
+type testConfig struct {
+	description         string
+	executeArgs         []string
+	processorSetupFunc  processorSetupFunc
+	expectedErrorString string
 }
 
-// SetBoolOption implements argle.Parser.
-func (p *testParser) SetBoolOption(opt string, v bool) error {
-	args := p.Called(opt, v)
-	return args.Error(0)
-}
-
-func TestSubcommandWithBoolOption(t *testing.T) {
-	p := argle.NewArgumentProcessor()
-	parser := testParser{}
-	p.AddSubcommand("subcommand", &parser)
-	p.ExecuteWithArgs([]string{"someprogram", "subcommand"})
+func Test(t *testing.T) {
+	testTable := []testConfig{
+		{
+			description: "bool flag with default",
+			processorSetupFunc: func(ap argle.ArgumentProcessor) {
+				ap.AddSubcommand("subcommand", argle.WithBoolOption("somebool"))
+			},
+			executeArgs: []string{"someprogram", "subcommand", "-somebool"},
+		},
+	}
+	for _, cfg := range testTable {
+		t.Run(cfg.description, func(t *testing.T) {
+			p := argle.NewArgumentProcessor()
+			cfg.processorSetupFunc(p)
+			err := p.ExecuteWithArgs(cfg.executeArgs)
+			if err != nil {
+				if cfg.expectedErrorString != "" {
+					assert.EqualError(t, err, cfg.expectedErrorString)
+				} else {
+					t.Error(err)
+				}
+			} else if cfg.expectedErrorString != "" {
+				t.Error("ExecuteWithArgs was expected to throw an error")
+			}
+		})
+	}
 }
