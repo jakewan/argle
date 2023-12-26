7e5922d4233e6665bc5aa29414cc74dacd8e2f53
Jacob Wan <jacobwan840@gmail.com>
2023-12-26 10:33:12 -0800

    wip

--- argle.go
+++ argle.go
@@ -1,8 +1,52 @@
 package argle
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-// Hello says hello.
-func Hello() {
-	fmt.Println("Hello there")
+type SubcommandHandler interface {
+	Exec()
+}
+
+type Config interface {
+	AddSubcommand(sc []string, h SubcommandHandler)
+	Parse()
+	ParseWithArgs(a []string)
+}
+
+type tempConfig struct {
+	handlers map[string]SubcommandHandler
+}
+
+func (c *tempConfig) Parse() {
+	c.ParseWithArgs(os.Args)
+}
+
+func (c *tempConfig) ParseWithArgs(a []string) {
+	fmt.Printf("ParseWithArgs given %s\n", a)
+	prog := a[0]
+	fmt.Printf("Program: %s\n", prog)
+	remaining := a[1:]
+	fmt.Printf("Remaining: %s\n", remaining)
+	key := strings.Join(remaining, " ")
+	h, ok := c.handlers[key]
+	if !ok {
+		fmt.Println("Handler not found")
+		return
+	}
+	h.Exec()
+}
+
+func (c *tempConfig) AddSubcommand(sc []string, h SubcommandHandler) {
+	fmt.Printf("Inside AddSubcommand\n")
+	key := strings.Join(sc, " ")
+	c.handlers[key] = h
+}
+
+func NewConfig() Config {
+	return &tempConfig{
+		handlers: map[string]SubcommandHandler{},
+	}
 }
--- examples/subcommands/main.go
+++ examples/subcommands/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/jakewan/argle"
+import (
+	"fmt"
+
+	"github.com/jakewan/argle"
+)
+
+type Shape int
+
+const (
+	circle Shape = iota
+	rectangle
+	triangle
+)
+
+type DrawShapesArgs struct {
+	count Shape
+}
+
+type DrawShapesHandler struct{}
+
+// Exec implements argle.SubcommandHandler.
+func (DrawShapesHandler) Exec() {
+	fmt.Println("Inside Exec")
+}
 
 func main() {
-	argle.Hello()
+	config := argle.NewConfig()
+	fmt.Printf("Argle config: %v\n", config)
+
+	var drawShapesArgs DrawShapesArgs
+	fmt.Printf("Draw shapes arg: %v\n", drawShapesArgs)
+
+	handler := DrawShapesHandler{}
+	config.AddSubcommand([]string{"draw", "shapes"}, handler)
+	config.Parse()
 }
